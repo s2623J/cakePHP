af47995e5527880db122e3a4633401bc37210bef
David Michael <devdavem@yahoo.com>
2022-05-28 12:28:43 -0700

    Added view elements to Home template

--- templates/Home/say_welcome.php
+++ templates/Home/say_welcome.php
@@ -6,17 +6,10 @@
 ?>
 
 <!-- Header-->
-<header class="py-5">
-    <div class="container px-lg-5">
-        <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
-            <div class="m-4 m-lg-5">
-                <h2 class="display-5 fw-bold">A warm welcome - Say Welcome!</h2>
-                <p class="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
-                <a class="btn btn-primary btn-lg" href="#!">Call to action</a>
-            </div>
-        </div>
-    </div>
-</header>
+<?= $this->element('jumbotron', [
+    "text" => "Say Welcome!"
+]); ?>
+
 <!-- Page Content-->
 <section class="pt-4">
     <div class="container px-lg-5">
